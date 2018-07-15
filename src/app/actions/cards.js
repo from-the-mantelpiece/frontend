c32a7917a3c85c2e0fe96cd0d7d914cafa933144
Catherine Looper <loopercat@gmail.com>
2018-07-15 15:02:58 -0700

    added card action

---
+++ src/app/actions/cards.js
@@ -0,0 +1,26 @@
+import uuid from 'uuid/v4';
+
+export const cardCreate = card => {
+
+  card.id = uuid();
+  card.createDate = new Date();
+
+  return {
+    type: 'CARD_ADD',
+    payload: card,
+  };
+};
+
+export const cardDelete = card => {
+  return {
+    type: 'CARD_DELETE',
+    payload: card,
+  };
+};
+
+export const cardUpdate = card => {
+  return {
+    type: 'CARD_UPDATE',
+    payload: card,
+  };
+};
